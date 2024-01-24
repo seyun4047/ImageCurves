cfa2beeb1175ecbbdd157236b47350d8695f0e91
seyun4047 <love4047@naver.com>
2024-01-25 00:54:41 +0900

    [update] 완성 color_set_creator 변화한 곡선 colorMapped: (list[x] = y) 를 기반으로 reImg(재구성 이미지)를 구성하고 반환
    1. 이미지를 array로 받아들임
    2. List[0-255] = [xn,yn]
    3. 색상변환: k->i
    4. tmp = list[k].좌표모두빼기(); list[i] = tmp;
    5. array재구성: List[0-255: t] -> xn,yn = t
    6. opencv로 반환

---
+++ ImageCurves/color_set_creator.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+
+# 1. 이미지를 array로 받아들임
+# 2. List[0-255] = [xn,yn]
+# 3. 색상변환: k->i
+# 4. tmp = list[k].좌표모두빼기(); list[i] = tmp;
+# 5. array재구성: List[0-255: t] -> xn,yn = t
+# 6. opencv로 반환
+
+# 1. 이미지를 array로 받아들임: oriImg
+# 2. List[0-255] = [xn,yn]
+def setImg(ih, iw, oriImg):
+    board = [[] for _ in range(256)]
+
+    for x in range(iw):
+        for y in range(ih):
+            pixColor = oriImg[y][x]
+            board[pixColor].append([x,y])
+
+    return board
+
+# 3. 색상변환: x->t
+# corlorMapped: 컬러
+# s: 변환 시작 포인트
+# e: 변환 종료 포인트
+# colorMapped[x] = y
+# board[color] = [[x1,y1], ... [xn, yn]]
+def processImg(board, s, e, colorMapped):
+    for x in range(s, e+1, 1):
+        t = colorMapped[x]
+        tLst = board[x]
+        board[x] = []
+        board[t] += (tLst)
+    return board
+
+# oriImg: 초기 이미지
+# s: 변환 시작 포인트
+# e: 변환 종료 포인트
+# board 변환된 이미지
+# dims: 컬러 채널수(지금은 흑백으로 진행하기때문에 1, rgb->3)
+def reloadImg(ih, iw, oriImg, s, e, board, dims):
+    # reImg = np.zeros((ih, iw, dims), dtype=np.uint8)
+    reImg = oriImg
+    for clrNum in range(s, e+1, 1):
+        tLst = board[clrNum]
+        for x, y in tLst:
+            reImg[y][x] = clrNum
+    # 재구성된 이미지 반환
+    return reImg
+
+#---------------------------------------------------
+# test
+# oriImg = cv2.imread("src/img/test2.png")
+# oriImg = cv2.cvtColor(oriImg, cv2.COLOR_BGR2GRAY)
+# ih, iw = oriImg.shape[:2]
+# board = setImg(ih, iw, oriImg)
+# s=0
+# e=100
+# cM = [0] * 256
+# # print(cM)
+# changedBoard = processImg(board, s, e, cM)
+# print(changedBoard[0])
+# reImg = reloadImg(ih, iw, oriImg, s, e, board=changedBoard, dims=1)
+# cv2.imshow('tmp',reImg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
--- ImageCurves/test.py
+++ ImageCurves/test.py
@@ -1,3 +1,110 @@
-preCurve = [i for i in range(0,256,1)]
-print(preCurve)
-print(len(preCurve))
+import cv2
+import sys
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
+from scipy.optimize import curve_fit
+# image setting with cv2
+oriImg = cv2.imread("src/img/test.JPG")
+oriImg = cv2.cvtColor(oriImg, cv2.COLOR_BGR2GRAY)
+ih, iw = oriImg.shape[:2]
+# cv2.imshow("img", oriImg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# print(iw,ih)
+# find that color(brightness) Algorithm
+
+#-----------------------------------------------------
+# 0~255 해당하는 좌표값 찾아서 리스트로 구성예정
+def findThatColor(cnum, ih, iw):
+    # use bfs
+    visited = [[0]*(ih+1) for _ in range(iw+1)]
+
+    # print(visited)
+    dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    q = deque()
+    q.append([0, 0])
+    visited[0][0] = 1
+    while q:
+        # print("hi")
+        cx, cy = q.popleft()
+        for dx,dy in dir:
+            # print("hi")
+            nx=cx+dx
+            ny=cy+dy
+            # print(nx,ny)
+            if 0<=nx<=iw-1 and 0<=ny<=ih-1 and visited[nx][ny]==0:
+                q.append([nx,ny])
+                # print("hi")
+                if oriImg[ny][nx] == cnum:
+                    print("(",nx,ny,")")
+                visited[nx][ny] = 1
+
+#-----------------------------------------------------
+# 커브 그리기
+# left는 right 보다 무조건 작다가정
+# def cubic_function(x, a, b, c, d):
+#     return a * x**3 + b * x**2 + c * x + d
+
+#-----------------------------------------------------
+# 방정식을 찾고 left dot pos, right dot pos, cur pos를 지나는 이차함수를 알아내 곡선을 그림
+def find_eq(x, a, b, c):
+    return a * x**2 + b * x + c
+
+#-----------------------------------------------------
+# curve의 Y값 이 변하면 지금 찍힌 점의 왼쪽, 오른쪽 점을 파악하고 선을 부드러운 곡선으로 만들기위해 변경되는 Y값을 반환함
+def transCurve(X, Y, userX, userY):
+    X.append(userX)
+    X.sort()
+    xIdx = X.index(userX)
+    tmpX = [i for i in range(X[xIdx-1]+1, X[xIdx+1], 1)]
+    newX = [X[xIdx-1], userX, X[xIdx+1]]
+    # print(X[xIdx-1], X[xIdx+1])
+    # print(Y[X[xIdx-1]], userY, Y[X[xIdx-1]])
+    newY = [Y[X[xIdx-1]], userY, Y[X[xIdx+1]]]
+
+    coef, _ = curve_fit(find_eq, newX, newY)
+    x_range = np.round(tmpX)
+    y_predicted = find_eq(x_range, *coef)
+    y_predicted = np.round(y_predicted)
+
+    return X, Y, X[xIdx-1], X[xIdx+1], y_predicted
+    # leftDotPos ~ curPos ~ rightDotPos 구간까지의 X에 대응하는 Y 를 반환하여
+    # 해당하는 컬러를 가진 픽셀의 컬러 변경
+    # lDot~rDot 사이의 길이를 알면 변경해야되는 컬러의 개수를 알수 있음.
+#-----------------------------------------------------
+# 원래 curveList와 변경되는 changedColorList를 가져와 색을 변환함.
+def changeCurve(curveList, changedColorList, lDotX, rDotX):
+    count=0
+    # colorRangeLen = rDotX - lDotX + 1
+    for colorIdx in range(lDotX+1, rDotX, 1):
+        curveList[colorIdx] = changedColorList[count]
+        count += 1
+    print(curveList)
+    return curveList
+
+
+#-----------------------------------------------------
+curveList = [i for i in range(0,256,1)] # CurveList x좌표: idx | y좌표: idx에 해당하는 값
+X = [0, 63, 127, 191, 255]
+
+#-----------------------------------------------------
+# test
+X, Y, lDotX, rDotX, changedColorList = transCurve(X,curveList,150,170) # X: 150인 값을 170으로 바꿨을 때
+curveList = changeCurve(curveList, changedColorList, lDotX, rDotX)
+
+X, Y, lDotX, rDotX, changedColorList = transCurve(X,curveList,100,200) # X: 150인 값을 170으로 바꿨을 때
+curveList = changeCurve(curveList, changedColorList, lDotX, rDotX)
+
+X, Y, lDotX, rDotX, changedColorList = transCurve(X,curveList,40,200) # X: 150인 값을 170으로 바꿨을 때
+curveList = changeCurve(curveList, changedColorList, lDotX, rDotX)
+
+#-----------------------------------------------------
+# Image Curve 시각화
+tmp_x = [i for i in range(0,256,1)]
+tmp_y = curveList
+clickedY = [tmp_y[i] for i in X]
+plt.plot(X, clickedY, "r.")
+plt.plot(tmp_x, tmp_y, "b-")
+plt.show()
